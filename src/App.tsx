388a9ee94161ff0f889e0cfd96a741bfe458bd17
PhanTrong1008 <63381972+PhanTrong1008@users.noreply.github.com>
2023-02-17 15:45:04 +0700

    Creating a new form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import logo from './assets/logo.svg';
+
 import styles from './App.module.scss';
+import { Input } from './components/input/input';
+import { Label } from './components/label/label';
+import { SubmitButton } from './components/submit-button/submit-button';
 
 function App() {
     return (
         <div className={styles.App}>
-            <header className={styles['App-header']}>
-                <img src={logo} className={styles['App-logo']} alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className={styles['App-link']}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <form>
+                <Label>Name</Label>
+                <Input id={'name'} name={'name'} />
+                <Label>Email</Label>
+                <Input id={'email'} name={'email'} />
+                <Label>Message</Label>
+                <Input id={'message'} name={'message'}></Input>
+                <SubmitButton />
+            </form>
         </div>
     );
 }
